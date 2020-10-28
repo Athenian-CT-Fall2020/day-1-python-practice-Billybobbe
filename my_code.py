2dc808e39c94b6e7db92afe25520bbf934ed3c5e
Billybobbe <24dmontgomery@athenian.org>
2020-10-28 09:31:21 -0700

    Day one notes.

--- my_code.py
+++ my_code.py
@@ -1,3 +1,36 @@
 # This is an example of a comment that Python ignores. Use these to make notes as you write your code. You will also fill out the top comment with any help or resources you use.
-# Collaborators:               (fill this is on every assignment, even if the answer is "none")
+# Collaborators: Just me             (fill this is on every assignment, even if the answer is "none")
 
+
+#comments:
+# Short comment
+'''Multi
+    line
+    comment'''
+
+
+
+# Printing to the terminal
+print("Hello world!")
+
+#variables: integer (int) , floating point/decimal (float), string (str), boolean (True/False)
+x = 5
+print(x)
+
+y = 3.1459
+print(y)
+
+name = "David"
+print(name)
+
+z = True
+print(z)
+
+print(type(z))
+
+print(f"Hello {name}!!")
+
+#user imput
+print("What is your favorite subject?       ")
+subject = input()
+print(f"{subject} is awesome!" )
